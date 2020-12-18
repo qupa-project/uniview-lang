67b16b2851d09411733b1f3d9f7c3961d5f13846
Ajani Bilby <me@ajanibilby.com>
2020-12-18 16:39:17 +1100

    Copied Syntax parsing from qupa-lang

---
+++ compiler/parser/build.js
@@ -0,0 +1,9 @@
+const BNF = require('bnf-parser');
+const fs = require('fs');
+
+let data = fs.readFileSync(__dirname+'/syntax.bnf', 'utf8');
+
+let syntax = BNF.Build(data, 'syntax.bnf');
+
+fs.writeFileSync(__dirname+'/syntax.json', JSON.stringify(syntax));
+console.info('Built Syntax');
