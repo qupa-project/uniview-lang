ccaf32fd83ba2ead399738d78b8cf71ea082a1f5
Ajani Bilby <me@ajanibilby.com>
2022-05-22 14:50:44 +1000

    +Build script for Ubuntu

---
+++ lib/install.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Save the directory of where the CLI was
+last=`pwd`
+
+# cd to the directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+# TODO: Update the CLI title to "Uniview Dependencie Setup - Configuring..."
+cmake -S . -B build --install-prefix="$SCRIPT_DIR/install" -DLLVM_TARGETS_TO_BUILD=X86
+# TODO: Update the CLI title to "Uniview Dependencie Setup - Building..."
+cmake --build build
+# TODO: Update the CLI title to "Uniview Dependencie Setup - Installing..."
+cmake --build build --target install
+
+# cd back to where the cli started this script from
+cd $last
