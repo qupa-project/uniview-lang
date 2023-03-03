9abdb7d06fc349bf7b43d404528cbd8442c7135a
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-03-03 12:24:30 +1100

    ~Removed Ninja Generator from requirement to default

--- lib/install.bash
+++ lib/install.bash
@@ -8,7 +8,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 # TODO: Update the CLI title to "Uniview Dependencie Setup - Configuring..."
-cmake -S . -B build --install-prefix="$SCRIPT_DIR/install" -DLLVM_TARGETS_TO_BUILD=X86
+cmake -S . -B build -G Ninja --install-prefix="$SCRIPT_DIR/install"
 # TODO: Update the CLI title to "Uniview Dependencie Setup - Building..."
 cmake --build build
 # TODO: Update the CLI title to "Uniview Dependencie Setup - Installing..."
