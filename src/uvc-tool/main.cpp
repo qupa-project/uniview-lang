f835b01a2f24237d7fc2937b702f377eddfd2921
Ajani Bilby <11359344+AjaniBilby@users.noreply.github.com>
2023-03-27 16:20:26 +1100

    +Bitcode and object write for uvc-tool2

--- src/uvc-tool/main.cpp
+++ src/uvc-tool/main.cpp
@@ -6,11 +6,28 @@ Config IngestConfig(int argc, char* argv[]) {
 	config.files.clear(); // init
 	config.mode = Exec_Mode::Object;
 	config.output = "out";
+	config.parseCoro = true;
+
+	// Normalize the target triple
+	// llvm::Triple triple(llvm::sys::getDefaultTargetTriple());
+	// printf("%s, %s, %s\n",
+	// 	triple.getArchName().str().c_str(),
+	// 	triple.getEnvironmentName().str().c_str(),
+	// 	triple.getOSName().str().c_str()
+	// );
+
+	// triple.setArch(llvm::Triple::normalizeArch(triple.getArch()));
+	// auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+	// module.setTargetTriple(TargetTriple);
+	// verbose("  target: %s\n", TargetTriple.c_str());
+
+	config.target = llvm::sys::getDefaultTargetTriple();
 
 	// Loop through each argument
 	for (int i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "--version") == 0) {
 			printf("Version: %s\n", "v0.0.0");
+			printf("  LLVM: %i.%i.%i\n\n", LLVM_VERSION_MAJOR, LLVM_VERSION_MINOR, LLVM_VERSION_PATCH);
 			continue;
 		}
 
@@ -20,36 +37,55 @@ Config IngestConfig(int argc, char* argv[]) {
 			continue;
 		}
 
-		if (strcmp(argv[i], "--output") != 1 && i+1 < argc) {
+		if (strcmp(argv[i], "--output") == 0 && i+1 < argc) {
 			config.output = argv[i+1];
 			i++;
 			continue;
 		}
 
-		if (strcmp(argv[i], "--mode") != 1 && i+1 < argc) {
+		if (strcmp(argv[i], "--target") == 0 && i+1 < argc) {
+			config.target = argv[i+1];
+			i++;
+			continue;
+		}
+
+		if (strcmp(argv[i], "--mode") == 0 && i+1 < argc) {
 			switch (argv[i+1][0]) {
 				case 'r':
 					config.mode = Exec_Mode::Run;
+					verbose("  Mode: Run\n");
 					break;
 				case 'i':
 					config.mode = Exec_Mode::IR;
+					verbose("  Mode: IR\n");
+					break;
+				case 'b':
+					config.mode = Exec_Mode::Bitcode;
+					verbose("  Mode: BC\n");
 					break;
 				case 'v':
 					config.mode = Exec_Mode::Verify;
+					verbose("  Mode: Verify\n");
 					break;
 				case 'o':
 					config.mode = Exec_Mode::Object;
+					verbose("  Mode: Object\n");
 					break;
+				default:
+					printf("Warn: Unknown mode %s\n", argv[i+1]);
 			}
 			i++;
 			continue;
 		}
 
+		verbose("  Attached input: %s\n", argv[i]);
 		config.files.push_back(argv[i]);
 	}
 
 	return config;
 }
+
+
 
 llvm::Module* loadAndLinkModules(llvm::LLVMContext& context, const std::vector<std::string>& files) {
 	llvm::SMDiagnostic error;
@@ -57,42 +93,172 @@ llvm::Module* loadAndLinkModules(llvm::LLVMContext& context, const std::vector<s
 
 	for (const auto& file : files) {
 		verbose("Parsing: %s\n", file.c_str());
-		std::unique_ptr<llvm::Module> module = llvm::parseIRFile(file, error, context);
-		if (!module) {
+		std::unique_ptr<llvm::Module> mod = llvm::parseIRFile(file, error, context);
+		if (!mod) {
 			error.print(/*Prompt=*/"", llvm::outs());
 			return nullptr;
 		}
 
-		verbose("  verifying...\n");
+		verbose("  - Verifying module\n");
 		std::string verifyErrors;
 		llvm::raw_string_ostream verifyStream(verifyErrors);
-		if (llvm::verifyModule(*module, &verifyStream)) {
-			llvm::errs() << "Module verification failed: " << verifyStream.str() << "\n";
+		if (llvm::verifyModule(*mod, &verifyStream)) {
+			llvm::errs() << "  Module verification failed: " << verifyStream.str() << "\n";
 			return nullptr;
 		}
 
 		if (main_module == nullptr) {
-			verbose("  main module\n");
-			main_module = module.get();
+			verbose("  - Found main module\n");
+			main_module = mod.release();
 		} else {
-			verbose("  linking...\n");
-			bool err = llvm::Linker::linkModules(*main_module, std::move(module));
+			verbose("  - Linking to main module\n");
+			bool err = llvm::Linker::linkModules(*main_module, std::move(mod));
 			if (err) {
-				verbose("  failed\n");
+				llvm::errs() << "  Module linking failed: " << verifyStream.str() << "\n";
 				return nullptr;
 			}
-			verbose("  linked\n");
 		}
 	}
 
-	verbose(" done\n");
 	return main_module;
 }
 
 
-int main(int argc, char* argv[]) {
-	verbose("Ingesting config\n");
 
+int Output_Module_Bitcode(llvm::Module* module, const std::string& file_path, bool bitcode) {
+	std::error_code ec;
+	llvm::raw_fd_ostream output(file_path, ec, llvm::sys::fs::OF_None);
+
+	if (ec) {
+		llvm::errs() << "Error opening file: " << ec.message() << "\n";
+		return 1;
+	}
+
+	if (bitcode) {
+		llvm::WriteBitcodeToFile(*module, output);
+	} else {
+		llvm::legacy::PassManager pm;
+		pm.add(llvm::createPrintModulePass(output));
+		pm.run(*module);
+		output.flush();
+	}
+
+
+	output.flush();
+	printf("Wrote: %s", file_path);
+	return 0;
+}
+
+
+
+int Compile_Object(llvm::LLVMContext& ctx, llvm::Module& module, const std::string& output_filename) {
+	// Initialize the target registry etc.
+	llvm::InitializeAllTargetInfos();
+	llvm::InitializeAllTargets();
+	llvm::InitializeAllTargetMCs();
+	llvm::InitializeAllAsmParsers();
+	llvm::InitializeAllAsmPrinters();
+
+	auto TargetTriple = module.getTargetTriple();
+	std::string err;
+	auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, err);
+	if (!Target) {
+		printf("\n\u001b[31merror\u001b[0m: %s\n", err.c_str());
+
+		printf("  Supported Targets:\n");
+		auto targets = llvm::TargetRegistry::targets();
+		for (auto it = targets.begin(); it != targets.end(); ++it) {
+			printf("    - %s\n", it->getName());
+		}
+
+		return 1;
+	}
+
+	auto CPU = "generic";
+	auto Features = "";
+
+	verbose("  - Starting target builder\n");
+	llvm::TargetOptions opt;
+	auto RM = llvm::Optional<llvm::Reloc::Model>();
+	auto TheTargetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+	module.setDataLayout(TheTargetMachine->createDataLayout());
+
+	std::error_code EC;
+	llvm::raw_fd_ostream dest(output_filename.c_str(), EC, llvm::sys::fs::OF_None);
+	if (EC) {
+		printf("  \u001b[31merror\u001b[0m: Could not open file \"%s\"\n", EC.message().c_str());
+		return 1;
+	}
+
+	llvm::legacy::PassManager pass;
+
+	auto FileType = llvm::CGFT_ObjectFile;
+	if (TheTargetMachine->addPassesToEmitFile(pass, dest, nullptr, FileType)) {
+		printf("  \u001b[31merror\u001b[0m: TheTargetMachine can't emit a file of this type\n");
+		return 1;
+	}
+
+	verbose("  - Verifying module\n");
+	llvm::verifyModule(module);
+
+	verbose("  - Running builder\n");
+	pass.run(module);
+
+	dest.flush();
+
+	verbose("\n");
+	printf("Wrote: %s\n", output_filename.c_str());
+	return 0;
+}
+
+
+
+int Execute_Module(llvm::LLVMContext& context, llvm::Module* module) {
+	verbose("Execution:\n");
+
+	// Find the main function in the module
+	llvm::Function* mainFunction = module->getFunction("main");
+	if (!mainFunction) {
+			printf("\n\u001b[31merror\u001b[0m: %s\n", "main function not found in module");
+			return 1;
+	}
+
+	// Verify the module to check for any errors
+	std::string error;
+	llvm::raw_string_ostream os(error);
+	if (llvm::verifyModule(*module, &os)) {
+		printf("\n\u001b[31merror\u001b[0m: %s\n  %s\n", "module verification failed with", error.c_str());
+		return 1;
+	}
+
+	// printf("\n\u001b[31merror\u001b[0m: %s\n", "Unimplemented");
+	// return 1;
+
+	// Create an execution engine for the module
+	std::unique_ptr<llvm::Module> modulePtr(module);
+	llvm::EngineBuilder builder( std::move(modulePtr) );
+	error.clear();
+	builder.setErrorStr(&error);
+	// builder.setEngineKind(llvm::EngineKind::JIT);
+	auto engine = builder.create();
+	if (!engine) {
+		printf("\n\u001b[31merror\u001b[0m: %s\n  %s\n", "Failed to create execution engine", error.c_str());
+		return 1;
+	}
+
+	// Execute the main function
+	std::vector<llvm::GenericValue> args;
+	llvm::GenericValue result = engine->runFunction(mainFunction, args);
+	auto code = result.IntVal.getSExtValue();
+
+	printf("\n\nExited: %i\n", code);
+	return code;
+}
+
+
+
+int main(int argc, char* argv[]) {
 	Config config = IngestConfig(argc, argv);
 
 	if (getVerbose()) {
@@ -103,7 +269,9 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	verbose("Reading files\n");
+	verbose("Target: %s\n", config.target.c_str());
+
+	verbose("Ingesting LLVM-IR\n");
 	llvm::LLVMContext context;
 	auto mod = loadAndLinkModules(context, config.files);
 	if (!mod) {
@@ -111,6 +279,33 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	std::cout << "Loaded and linked main module.\n";
+	mod->setTargetTriple(config.target);
+
+	if (config.parseCoro) {
+		// Apply coroutine passes
+	}
+
+	switch (config.mode) {
+		case Exec_Mode::Verify:
+			verbose("Verifying...");
+			llvm::verifyModule(*mod);
+			break;
+		case Exec_Mode::IR:
+		verbose("Writing LLVM-IR");
+			Output_Module_Bitcode(mod, config.output + ".ll", false);
+			break;
+		case Exec_Mode::Bitcode:
+			verbose("Writing Bitcode");
+			Output_Module_Bitcode(mod, config.output + ".bc", true);
+			break;
+		case Exec_Mode::Run:
+			return Execute_Module(context, mod);
+		case Exec_Mode::Object:
+			verbose("Compiling object:\n");
+			Compile_Object(context, *mod, config.output + ".o");
+			break;
+	}
+
+	verbose("\n");
 	return 0;
 }
--- src/uvc-tool/main.hpp
+++ src/uvc-tool/main.hpp
@@ -2,30 +2,50 @@
 
 #include <iostream>
 #include <stdio.h>
-#include <vector>
 #include <string>
+#include <vector>
 
+
+#include <llvm/ADT/Triple.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
+#include <llvm/CodeGen/CommandFlags.h>
+#include <llvm/CodeGen/TargetPassConfig.h>
+#include <llvm/Config/llvm-config.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+// #include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRPrintingPasses.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Verifier.h>
 #include <llvm/IRReader/IRReader.h>
 #include <llvm/Linker/Linker.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Passes/PassBuilder.h>
+#include <llvm/Support/CodeGen.h>
 #include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Host.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Target/TargetMachine.h>
+#include <llvm/Target/TargetOptions.h>
+#include <llvm/Transforms/Coroutines/CoroCleanup.h>
+#include <llvm/Transforms/Coroutines/CoroConditionalWrapper.h>
+#include <llvm/Transforms/Coroutines/CoroEarly.h>
+#include <llvm/Transforms/Coroutines/CoroSplit.h>
 #include <llvm/Transforms/IPO.h>
-
-#include <vector>
-#include <string>
-#include <iostream>
+#include <llvm/Transforms/IPO/PassManagerBuilder.h>
 
 #include "verbose.hpp"
 
 enum class Exec_Mode {
 	Run,
 	IR,
+	Bitcode,
 	Object,
 	Verify
 };
@@ -34,9 +54,20 @@ struct Config {
 	Exec_Mode                mode;
 	std::vector<std::string> files;
 	std::string              output;
+	std::string              target;
+	bool                 coroutines;
 };
 
+Config IngestConfig(int argc, char* argv[]);
 
 llvm::Module* loadAndLinkModules(llvm::LLVMContext& context, const std::vector<std::string>& files);
-Config IngestConfig(int argc, char* argv[]);
+
+int Output_Module_Bitcode(llvm::Module* module, const std::string& file_path, bool bitcode);
+
+// void ApplyCoroutine(llvm::LLVMContext& context, llvm::Module& module);
+
+int Execute_Module(llvm::LLVMContext& context, llvm::Module* module);
+
+int Compile_Object(llvm::LLVMContext& ctx, llvm::Module& module, const std::string& output_filename);
+
 int main(int argc, char* argv[]);
